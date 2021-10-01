472fe674d26f65de0b3a1543c6d22be29ec725e5
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-10-01 16:47:35 +0100

    added method signatures from visualisation and curriculum wrapper to base wrapper to ensure both can be used compositionally

--- key_door/wrapper.py
+++ key_door/wrapper.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 from key_door import base_environment
@@ -11,8 +11,10 @@ class Wrapper(base_environment.BaseEnvironment):
     def step(self, action) -> Tuple[float, Tuple[int, int]]:
         return self._env.step(action)
 
-    def reset_environment(self, train: bool = True) -> Tuple[int, int, int]:
-        return self._env.reset_environment(train=train)
+    def reset_environment(
+        self, train: bool = True, map_yaml_path: Optional[str] = None
+    ) -> Tuple[int, int, int]:
+        return self._env.reset_environment(train=train, map_yaml_path=map_yaml_path)
 
     @property
     def active(self) -> bool:
@@ -57,3 +59,31 @@ class Wrapper(base_environment.BaseEnvironment):
     @property
     def test_episode_position_history(self) -> np.ndarray:
         return np.array(self._env._test_episode_position_history)
+
+    # including these in the base wrapper is not ideal. Maybe rethink wrappers...
+    def render(
+        self,
+        save_path: Optional[str] = None,
+        dpi: Optional[int] = 60,
+        format: str = "state",
+    ) -> None:
+        self._env.render(save_path=save_path, dpi=dpi, format=format)
+
+    def visualise_episode_history(
+        self, save_path: str, history: Union[str, List[np.ndarray]] = "train"
+    ) -> None:
+        self._env.visualise_episode_history(save_path=save_path, history=history)
+
+    def plot_heatmap_over_env(
+        self,
+        heatmap: Dict[Tuple[int, int], float],
+        fig: Optional = None,
+        ax: Optional = None,
+        save_name: Optional[str] = None,
+    ) -> None:
+        self._env.plot_heatmap_over_env(
+            heatmap=heatmap, fig=fig, ax=ax, save_name=save_name
+        )
+
+    def __next__(self) -> None:
+        next(self._env)
